f55e10a04f12db38c84fb8574c2c17c2263e078e
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-06-21 12:12:49 +0600

    Delete Product.cs

--- Product.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversityManagementSystem.Models
-{
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public int Credits { get; set; }
-    }
-}
