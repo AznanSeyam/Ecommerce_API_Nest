14ac63be5f35551b2d7ab346fafdcc961f71a12d
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-06-21 12:12:03 +0600

    Delete Cart.cs

--- Cart.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversityManagementSystem.Models
-{
-    public class Student
-    {
-        
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
