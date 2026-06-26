d8bb2aae07296e52d34e783a2760533ff4a2ce21
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-06-26 13:43:43 +0600

    feat:database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
